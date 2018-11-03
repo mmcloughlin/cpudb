41a88be729b0ab245a15c944b76717c175e680e2
Michael McLoughlin <mmcloughlin@gmail.com>
2018-11-03 16:30:25 -0700

    add cpuid query function

--- core.go
+++ core.go
@@ -9,9 +9,19 @@ type CPU struct {
 	Leaves   map[uint32][]Leaf
 }
 
+// Leaf contains values from one CPUID leaf.
 type Leaf struct {
 	EAX uint32
 	EBX uint32
 	ECX uint32
 	EDX uint32
+}
+
+// CPUID simulates a CPUID call on this CPU. Boolean return value is false if the leaf does not exist.
+func (c *CPU) CPUID(eax, ecx uint32) (Leaf, bool) {
+	sub, ok := c.Leaves[eax]
+	if !ok || int(ecx) >= len(sub) {
+		return Leaf{}, false
+	}
+	return sub[ecx], true
 }
